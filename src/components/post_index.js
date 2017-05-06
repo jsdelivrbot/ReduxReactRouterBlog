f77a2f8d276352dd4268c9e95ceb8b9217abf68d
“Richard <richgurney1987@gmail.com>
2017-05-06 12:45:01 +0100

    added a form with redux-form with validation

--- src/components/post_index.js
+++ src/components/post_index.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount(){
-    console.log('this would be a good time to call and action creator');
+    // When the page opens do the call to get the posts
+    this.props.fetchPosts();
   }
   render() {
     return (
-      <div>List of blog posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
+        List of blog posts
+      </div>
+
     );
   }
 }
 
-export default PostsIndex
+// function mapDispatchToProps(dispath) {
+//   return bindActionCreators({ fetchPosts }, dispath );
+// }
+
+export default connect(null, { fetchPosts })(PostsIndex);
